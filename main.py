315d5cdf4c6eef9c5876b22f099568e770060013
Shubhang Srikoti <70000106+shubhangsrikoti@users.noreply.github.com>
2020-09-18 20:29:35 +0000

    .

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Author: Shubhang Srikoti svs6959@psu.print
+# Author: Shubhang Srikoti svs6959@psu.edu
 
 def digit_sum(number):
   if number == 0:
@@ -6,8 +6,9 @@ def digit_sum(number):
   else:
     return (number%10) + digit_sum(number//10)
 
+
 def run():
-  number = input("Enter an int: ")
+  number = int(input("Enter an int: "))
   print(f"sum of digits of {number} is {digit_sum(number)}.")
 
 if __name__ == "__main__":
